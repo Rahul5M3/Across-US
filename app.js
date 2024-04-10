beb1f9e5dfd55f3e9ee15d9afbb2263e6c004873
Rahul5M3 <rahulseervi2003@gmail.com>
2024-04-10 14:17:44 +0530

    project file uploaded

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const ejs=require('ejs');
+const ejsMate=require('ejs-mate');
+const methodOverride=require('method-override');
+
+app.use(express.static(path.join(__dirname,'/public')));
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine','ejs');
+app.use(methodOverride('_method'))
+app.engine('ejs',ejsMate)
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.listen(8080,()=>{
+    console.log('server started');
+})
+
+app.get('/',(req,res)=>{
+    res.send('working');
+})
+
+app.get('/login',(req,res)=>{
+    res.render("login.ejs");
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup.ejs');
+})
---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded',(event)=>{
+//     const imgDiv=document.getElementById('content');
+//     setTimeout(()=>{
+//         imgDiv.style.opacity=1;
+//     },700);
+// })
+
+
+const c=document.querySelector('.img-content');
+c.addEventListener('click',()=>{
+    const co=document.getElementById('content-2');
+    co.style.color='black';
+});
