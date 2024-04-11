d2f49f6d84c5093266087eeb1338c54e1acd2e7f
Rahul5M3 <rahulseervi2003@gmail.com>
2024-04-11 16:35:38 +0530

    new files

--- public/js/home.js
+++ public/js/home.js
@@ -12,3 +12,22 @@ function scrollContainer(amount){
     homeMainContent.scrollLeft += amount;
 }
 
+const menuBar=document.querySelector('.menuBar-box');
+const menuBox=document.querySelector('.menuBox');
+menuBar.addEventListener('click',()=>{
+    if(menuBox.style.display==='none'){
+        menuBox.style.display='block';
+        menuBar.style.zIndex='10';      
+    }else {
+        menuBox.style.display='none';
+        menuBar.style.zIndex='2';
+    }
+});
+
+
+function scrollToSection(sectionId){
+    const sectionid=document.getElementById('sectionId');
+    if(sectionid){
+        sectionid.scrollIntoView({behavior:"smooth"});
+    }
+}
